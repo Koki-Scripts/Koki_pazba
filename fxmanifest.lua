bccecd5c71ce8ea92bb0488e9656f3328ccf13bf
Koki-Scripts <fivemucet12@gmail.com>
2025-08-17 19:07:28 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Koki-Scripts'
+description 'Script pro FiveM, který umožňuje pomocí příkazu /pažba zapnout nebo vypnout úder pažbou zbraně.'
+version '1.0.0'
+
+client_script 'client.lua'
